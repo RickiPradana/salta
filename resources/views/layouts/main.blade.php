4cabdce575639d17d276cb6d9863866d90e2e9a3
RICKI PRADANA <ricki.pradana@krl.co.id>
2024-09-21 18:37:33 +0700

    add meta tag apple mobile

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,6 +17,10 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+
     <meta name="description"
         content="Cara mengawetkan tahu dengan berbagai metode, baik alami maupun tradisional. Temukan tips menyimpan tahu agar tetap segar dan tahan lama.">
     <meta name="keywords"
