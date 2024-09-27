8ade9c25b5b7892334cbd32fc67e868515e51041
RICKI PRADANA <ricki.pradana@krl.co.id>
2024-09-27 22:02:31 +0700

    add visitorlobrary

---
+++ app/Http/Middleware/CountryMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Torann\GeoIP\Facades\GeoIP;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
+
+class CountryMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $visitorData = GeoIP::getLocation();
+        
+        DB::table('t_visitor')->insert([
+            'ip_address' => $request->ip(),
+            'country' => $visitorData['country'],
+            'visited_at' => now(),
+        ]);
+
+        return $next($request);
+        // return $next($request);
+    }
+}
